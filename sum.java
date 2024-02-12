6e897a9ced716040045a060be351339e14293650
Sonali Gaikwad <sonali.gaikwad@qualitykiosk.com>
2024-02-12 21:42:09 +0530

    Adding in sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,7 @@
-public class Sum {
-    public static void main(String[]args)
+public class sum {
+    public static void main(String[]args){
     int a= 2;
     int b= 3;
-    System.out.println(a+b);
+    System.out.println("The sum is " + (a+b));
+}
 }
